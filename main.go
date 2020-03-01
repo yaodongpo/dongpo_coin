de39c758de5913f855c1125c30b02898eda85492
yaodongpo <893177204@qq.com>
2020-03-01 21:25:50 +0800

    新增区块的链结构

--- main.go
+++ main.go
@@ -21,16 +21,35 @@ func ComputeHash(data string) string {
 }
 
 //链，区块的单链
-type Chain []Block
+type Chain struct {
+	chain []Block
+}
 
 //生成区块链的时候初始化创世区块
 func NewChain() *Chain {
-	block0 := NewBlock("I am the fuck 创世区块", "")
-	return &Chain{*block0}
+	block0 := NewBlock("I am the fuck 创世区块", "") //创世区块没有前区块
+	chain := []Block{*block0}
+	return &Chain{chain}
+}
+
+//根据下标获取区块
+func (chain Chain) GetIndex(index int8) Block {
+	return chain.chain[index]
+}
+
+//新增区块到链上
+func (chain *Chain) AddBlock(block Block) error {
+	chain.chain = append(chain.chain, block)
+	return nil
 }
 
 //测试main函数
 func main() {
 	chain := NewChain()
 	fmt.Println("chain:", chain)
+	fmt.Println("chain[0]:", chain.GetIndex(0))
+	block1 := NewBlock("测试新增区块到链上！", chain.GetIndex(0).Hash)
+	chain.AddBlock(*block1)
+	fmt.Println("chain 增加区块后:", chain)
+
 }
